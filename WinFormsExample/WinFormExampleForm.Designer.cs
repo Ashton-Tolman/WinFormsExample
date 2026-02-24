03fd400f6d31e8337d97080d322422e46005f9c2
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-02-24 08:21:57 -0700

    Add controls and fix tab stop

--- WinFormsExample/WinFormExampleForm.Designer.cs
+++ WinFormsExample/WinFormExampleForm.Designer.cs
@@ -32,6 +32,13 @@
             SubmitButton = new Button();
             NameLabel = new Label();
             NameTextBox = new TextBox();
+            AgeTextBox = new TextBox();
+            AgeLabel = new Label();
+            CityTextBox = new TextBox();
+            CityLabel = new Label();
+            PhoneTextBox = new TextBox();
+            PhoneLabel = new Label();
+            ClearButton = new Button();
             SuspendLayout();
             // 
             // ExitButton
@@ -39,17 +46,17 @@
             ExitButton.Location = new Point(634, 363);
             ExitButton.Name = "ExitButton";
             ExitButton.Size = new Size(154, 75);
-            ExitButton.TabIndex = 0;
+            ExitButton.TabIndex = 6;
             ExitButton.Text = "E&xit";
             ExitButton.UseVisualStyleBackColor = true;
             ExitButton.Click += ExitButton_Click;
             // 
             // SubmitButton
             // 
-            SubmitButton.Location = new Point(465, 363);
+            SubmitButton.Location = new Point(286, 363);
             SubmitButton.Name = "SubmitButton";
             SubmitButton.Size = new Size(163, 75);
-            SubmitButton.TabIndex = 1;
+            SubmitButton.TabIndex = 4;
             SubmitButton.Text = "&Submit";
             SubmitButton.UseVisualStyleBackColor = true;
             SubmitButton.Click += SubmitButton_Click;
@@ -57,7 +64,7 @@
             // NameLabel
             // 
             NameLabel.AutoSize = true;
-            NameLabel.Location = new Point(24, 9);
+            NameLabel.Location = new Point(24, 15);
             NameLabel.Name = "NameLabel";
             NameLabel.Size = new Size(59, 25);
             NameLabel.TabIndex = 2;
@@ -68,13 +75,77 @@
             NameTextBox.Location = new Point(89, 12);
             NameTextBox.Name = "NameTextBox";
             NameTextBox.Size = new Size(150, 31);
-            NameTextBox.TabIndex = 3;
+            NameTextBox.TabIndex = 0;
+            // 
+            // AgeTextBox
+            // 
+            AgeTextBox.Location = new Point(89, 48);
+            AgeTextBox.Name = "AgeTextBox";
+            AgeTextBox.Size = new Size(150, 31);
+            AgeTextBox.TabIndex = 1;
+            // 
+            // AgeLabel
+            // 
+            AgeLabel.AutoSize = true;
+            AgeLabel.Location = new Point(24, 51);
+            AgeLabel.Name = "AgeLabel";
+            AgeLabel.Size = new Size(44, 25);
+            AgeLabel.TabIndex = 4;
+            AgeLabel.Text = "Age";
+            // 
+            // CityTextBox
+            // 
+            CityTextBox.Location = new Point(89, 124);
+            CityTextBox.Name = "CityTextBox";
+            CityTextBox.Size = new Size(150, 31);
+            CityTextBox.TabIndex = 3;
+            // 
+            // CityLabel
+            // 
+            CityLabel.AutoSize = true;
+            CityLabel.Location = new Point(24, 127);
+            CityLabel.Name = "CityLabel";
+            CityLabel.Size = new Size(42, 25);
+            CityLabel.TabIndex = 8;
+            CityLabel.Text = "City";
+            // 
+            // PhoneTextBox
+            // 
+            PhoneTextBox.Location = new Point(89, 88);
+            PhoneTextBox.Name = "PhoneTextBox";
+            PhoneTextBox.Size = new Size(150, 31);
+            PhoneTextBox.TabIndex = 2;
+            // 
+            // PhoneLabel
+            // 
+            PhoneLabel.AutoSize = true;
+            PhoneLabel.Location = new Point(24, 91);
+            PhoneLabel.Name = "PhoneLabel";
+            PhoneLabel.Size = new Size(62, 25);
+            PhoneLabel.TabIndex = 6;
+            PhoneLabel.Text = "Phone";
+            // 
+            // ClearButton
+            // 
+            ClearButton.Location = new Point(455, 363);
+            ClearButton.Name = "ClearButton";
+            ClearButton.Size = new Size(169, 75);
+            ClearButton.TabIndex = 5;
+            ClearButton.Text = "Clear";
+            ClearButton.UseVisualStyleBackColor = true;
             // 
             // WinFormExampleForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(ClearButton);
+            Controls.Add(CityTextBox);
+            Controls.Add(CityLabel);
+            Controls.Add(PhoneTextBox);
+            Controls.Add(PhoneLabel);
+            Controls.Add(AgeTextBox);
+            Controls.Add(AgeLabel);
             Controls.Add(NameTextBox);
             Controls.Add(NameLabel);
             Controls.Add(SubmitButton);
@@ -91,5 +162,12 @@
         private Button SubmitButton;
         private Label NameLabel;
         private TextBox NameTextBox;
+        private TextBox AgeTextBox;
+        private Label AgeLabel;
+        private TextBox CityTextBox;
+        private Label CityLabel;
+        private TextBox PhoneTextBox;
+        private Label PhoneLabel;
+        private Button ClearButton;
     }
 }
